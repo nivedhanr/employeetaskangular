818de9f0f3328512e7a964934c6757c72a6beec4
nivedhanr <nivedharenahan@gmail.com>
2022-12-11 00:56:46 +0530

    till view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserregComponent } from './userreg/userreg.component';
+import { ViewprofileComponent } from './viewprofile/viewprofile.component';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminloginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent,
+    UserloginComponent,
+    UserregComponent,
+    ViewprofileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(myRoute),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
